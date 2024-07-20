13a8699965cd286ff75e3d92c498db83fba19dc7
chhorvorn10 <vychhorvorn@gmail.com>
2024-07-20 11:03:49 +0700

    second commit

---
+++ test_datatype.py
@@ -0,0 +1,10 @@
+# str = 'hello world!'
+#
+# print(str + " from BBU")
+# print(str * 2)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+
+a=100.55
+print("the type of variable having values", a, " is ", type(a))
