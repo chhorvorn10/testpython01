89aa99accd58c4c2dfd1c386422d86f3927dffbc
chhorvorn10 <vychhorvorn@gmail.com>
2024-07-20 10:17:01 +0700

    first commit

---
+++ App.py
@@ -0,0 +1,55 @@
+# print("Hello, World!")
+# print("Double \"quote\" and single \'quote\'")
+# print("Hello "+str(9))
+# name = input("What is your name?")
+# print("welcome " + name)
+
+# print("Welcome amount calculator")
+# qty1 = 10
+# qty2 = 10
+# price = 20
+# amount = (qty1 + qty2) * price
+#
+# print("Total Qty: " + str(qty1+qty2))
+# print("Total Amount: " + str(amount))
+
+# print("Welcome amount calculator")
+# qty1 = int(input("Enter qty 1: "))
+# qty2 = int(input("Enter qty 2: "))
+# price = float(input("Enter Price: "))
+#
+# total_qty = qty1 + qty2
+# amount = (qty1 + qty2) * price
+#
+# print("--------------------------------")
+# print("Total Qty: " + str(total_qty))
+# print("Total Amount: " + str(amount))
+
+# name = "Chhorvorn"
+# print(len(name))
+
+# a = input("Enter Name 1: ")
+# b = input("Enter Name 2: ")
+#
+# temp = a
+# a = b
+# b = temp
+#
+# print("a: " + a)
+# print("b: " + b)
+
+# print("Welcome to the band name generator")
+#
+# city = input("Which city did you grow up in ?\n")
+# pet = input("What is the name of a pet ?\n")
+#
+# print("Your band name could be " + city +" "+ pet)
+
+print("Welcome to the band name generator")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+
+band_name = f"Your band name could be {city} {pet}"
+
+print(band_name)
